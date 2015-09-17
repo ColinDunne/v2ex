031dc466b17497baa9a7475482480d3f553272b5
QianChen <xgdjqc@126.com>
2015-09-17 16:17:50 +0800

    初始化项目

---
+++ v2ex/v2ex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  v2ex
+//
+//  Created by 钱辰 on 15/9/17.
+//  Copyright © 2015年 com.qianchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ v2ex/v2ex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  v2ex
+//
+//  Created by 钱辰 on 15/9/17.
+//  Copyright © 2015年 com.qianchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
